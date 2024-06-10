e4c3867745906eb625e5ba6086140bab6941dc46
Enes Balay <enesbalay@outlook.com>
2024-06-10 12:20:55 +0300

    Update DB Migration

---
+++ DataAccessLayer/Migrations/20231128060654_first.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
